30e45668ec06cb1d21e4e7683313fa65e438d4b6
Renata Freitas <r.miriuk@gmail.com>
2019-11-15 15:34:03 +0000

    Automatically backed up by Learn

---
+++ Trash.rb
@@ -0,0 +1,215 @@
+def hash_has_blank(hsh)
+  collection.each do |k,v|
+    if v.include?(name)
+      return v
+    end
+  end
+  return nil
+
+
+ if collection.include?(name)
+   #how to return the hash that has the name in it?
+   return collection[name]
+ else
+   return nil
+ end
+
+ collection.each do |key,value|
+   if value.include?(name)
+     return collection
+   end
+ end
+ return nil
+
+ i = 0
+ myCart = []
+ myCart.push(cart)
+
+ while i < myCart.length do
+
+     if myCart[i].has_key?(:count)
+       myCart[i][:count] += 1
+     else
+       myCart[i][:count] = 1
+     end
+     i += 1
+ end
+
+ while cart_i < cart.length do
+   #my problem here is how to compare if a name exist in the array cart,
+   #and if yes it will make a count of 2 (or more)
+   myCart.push(cart[cart_i])
+     if myCart[myCart_i].include? cart[cart_i] do
+       if myCart[i].has_key?(:count)
+         myCart[i][:count] += 1
+       else
+         myCart[i][:count] = 1
+       end
+       i += 1
+ end
+
+ while cart_i < cart.length do
+   #my problem here is how to compare if a name exist in the array cart,
+   #and if yes it will make a count of 2 (or more)
+   myCart.push(cart)
+     if myCart.include?(cart[cart_i])
+       if myCart[myCart_i].has_key?(:count)
+         myCart[myCart_i][:count] += 1
+       end
+     else
+       myCart[myCart_i][:count] = 1
+     end
+             myCart_i += 1
+     cart_i += 1
+ end
+ return  myCart
+
+
+ myCart.push(cart[cart_i])
+ myCart[myCart_i][:count] = 1
+     unless myCart.include?(cart[cart_i])
+         myCart[cart_i][:count] = 1
+       else
+         myCart[myCart_i][:count] += 1
+     # else
+     #   myCart[myCart_i][:count] = 1
+     end
+
+     myCart_i += 1
+     cart_i += 1
+ end
+ myCart
+
+
+ myCart_i = 0
+ cart_i = 0
+ myCart = []
+
+ while cart_i < cart.length do
+   #my problem here is how to compare if a name exist in the array cart,
+   #and if yes it will make a count of 2 (or more)
+   #add cart_i
+ #  cart[cart_i][:count] = 1
+ myCart.push(cart[cart_i])
+ myCart[myCart_i][:count] = 1
+     unless myCart.include?(cart[cart_i]) #unless works as if does not include
+       myCart.push(cart[cart_i])
+       myCart[myCart_i][:count] += 1
+     # else
+     #   myCart[myCart_i][:count] = 1
+     end
+     myCart_i += 1
+     cart_i += 1
+ end
+
+
+ newCart_i = 0
+
+ while newCart_i < newCart.length do
+   newCart[newCart_i][:count] = 1
+   unless find_item_by_name_in_collection(newCart[newCart_i],newCart).include?(newCart)
+     newCart[newCart_i][:count] += 1
+   end
+     newCart_i += 1
+ end
+
+
+
+ def consolidate_cart(cart)
+
+   myCart_i = 0
+   cart_i = 0
+   myCart = []
+
+   while cart_i < cart.length do
+
+   myCart.push(cart[cart_i])
+       if myCart.include?(cart[cart_i])
+         myCart[myCart_i][:count] += 1
+       else
+          myCart[myCart_i][:count] = 1
+       end
+       myCart_i += 1
+       cart_i += 1
+   end
+
+ end
+
+ cart[i_cart][:count] = cart[i_cart][:count] - coupons[i_coupons][:num]
+coupons[i_coupons][:item] += " W/COUPON"
+cart.push(coupons[i_coupons])
+
+
+##########################################
+cart_1=[
+  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
+  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
+]
+coupons_1 = [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
+
+def apply_coupons(cart, coupons)
+n_of_itens = 0
+i_cart = 0
+while i_cart < cart.length do
+i_coupons = 0
+  if cart.any? {|h| h[:item] == coupons[i_coupons][:item] && h[:count] >= coupons[i_coupons][:num]}
+
+
+    itemWDisc = Hash.new
+    itemWDisc = coupons[i_coupons]
+    itemWDisc[:item] += " W/COUPON"
+    #itemWDisc[:count] = 0
+    coupon_cost = coupons[i_coupons][:cost]
+    coupon_num = coupons[i_coupons][:num]
+    itemWDisc[:price] = coupon_cost/coupon_num
+
+    cart << itemWDisc
+
+    cart[i_cart][:count] -=  coupons[i_coupons][:num]
+
+    p "it prints"
+    print cart
+  end
+  i_cart += 1
+  i_coupons += 1
+
+end
+
+
+
+end
+
+apply_coupons(cart_1, coupons_1)
+
+
+return false if cart.empty? ### did nothing
+while i_cart < cart.length do
+
+  i_coupons = 0
+  if cart[i_cart][:item] == coupons[i_coupons][:item] && cart[i_cart][:count] >= coupons[i_coupons][:num]
+
+    cart[i_cart][:count] = cart[i_cart][:count] - coupons[i_coupons][:num]
+
+   itemWDisc = Hash.new
+   itemWDisc.merge!(cart[i_cart])
+   itemWDisc[:item] = itemWDisc[:item] + " W/COUPON"
+
+   itemWDisc[:count] = coupons[i_coupons][:num]
+   itemWDisc[:price] = coupons[i_coupons][:cost]/coupons[i_coupons][:num]
+
+   cart.push(itemWDisc)
+ end
+ i_cart += 1
+ i_coupons += 1
+end
+p cart
+
+##########################
+
+i = 0
+while i < collection.length do
+  if collection[i][:item].include?(name)
+    return collection[i]
+  end
+  i += 1
+end
--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,15 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+
+i = 0
+while i < collection.length do
+  if collection[i][:item].include?(name)
+    return collection[i]
+  end
+  i += 1
+end
+
 end
 
 def consolidate_cart(cart)
@@ -9,18 +18,100 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+
+#rspec spec/grocer_spec.rb:49
+
+  # * Arguments:
+  #   * `Array`: a collection of item Hashes
+  # * Returns:
+  #   * a ***new*** `Array` where every ***unique*** item in the original is present
+  #     * Every item in this new `Array` should have a `:count` attribute
+  #     * Every item's `:count` will be _at least_ one
+  #     * Where multiple instances of a given item are seen, the instance in the
+  #       new `Array` will have its `:count` increased
+  myCart_i = 0
+  cart_i = 0
+  myCart = []
+  #while cart_i < cart.length do
+while cart_i < cart.length do
+cart[cart_i][:count] = 1
+
+#myCart[myCart_i][:count] = 1
+      if myCart.any? {|h| h[:item] == cart[cart_i][:item]}
+          myCart.each do |item|
+          item[:count] = cart[cart_i][:count]
+          item[:count] += 1
+          end
+       else
+         myCart.push(cart[cart_i])
+
+
+       end
+       myCart_i += 1
+       cart_i += 1
+       end
+       p myCart
+
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  # * Arguments:
+  #   * `Array`: a collection of item `Hash`es
+  #   * `Array`: a collection of coupon `Hash`es
+  # * Returns:
+  #   * A ***new*** `Array`. Its members will be a mix of the item `Hash`es and,
+  #     where applicable, the "ITEM W/COUPON" `Hash`. Rules for application are
+  #     described below.
+
+  #rspec spec/grocer_spec.rb:77
+
+  #If there are 2 avocados it will be 2.50 each, and the item name will change also.
+  #it might need to compare the new hash with the coupons one.
+i_coupons = 0
+while i_coupons < coupons.length
+  cart_item = find_item_by_name_in_collection(coupons[i_coupons][:item], cart)
+  coupon_item_name = "#{coupons[i_coupons][:item]}  W/COUPON"
+  cart_item_wcoupon = find_item_by_name_in_collection(coupon_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[i_coupons][:num]
+      if cart_item_wcoupon
+        cart_item_wcoupon += coupons[i_coupons][:num]
+        cart_item -= coupons[i_coupons][:num]
+      else
+        cart_item_wcoupon = {
+          :item => cart_item,
+          :price => coupons[i_coupons][:cost] / coupons[i_coupons][:num],
+          :count => coupons[i_coupons][:num],
+          :clearance => coupons[i_coupons][:clearance]
+        }
+        cart << cart_item_wcoupon
+        cart_item[:count] -= coupons[i_coupons][:num]
+      end
+    end
+
+  i_coupons += 1
+end
+
+
+
+#######Not finished!!!! I have no idea of what to do
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
-  #
+  #rspec spec/grocer_spec.rb:196
   # REMEMBER: This method **should** update cart
+  cart_i = 0
+while cart_i < cart.length do
+    if cart.any? {|arr| arr[:clearance] == true }
+      cart[cart_i][:price] = cart[cart_i][:price] - (cart[cart_i][:price] * 0.2)
+      cart[cart_i][:price].round(2)
+    end
+    cart_i += 1
+    p cart
+  end
 end
 
 def checkout(cart, coupons)
